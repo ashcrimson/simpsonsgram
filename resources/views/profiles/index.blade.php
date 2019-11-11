29055978456df078ad8feaad38b8c4e5bc67efe8
Felipe Pino <felipepinoredes@gmail.com>
2019-11-11 20:58:14 -0300

    Cambiar username a name

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-2">
-                    <div class="h4">{{ $user->username }}</div>
+                    <div class="h4">{{ $user->name }}</div>
 
                     <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
                 </div>
